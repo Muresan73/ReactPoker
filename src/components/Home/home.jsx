722ecd995d5bfb5f977817dbb4541542d7ee8051
e-mog <modi.g@windowslive.com>
2019-02-17 11:48:35 +0100

    proxy 2 express

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,10 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./home.scss";
+import axios from "axios";
 
-const HomePage = () => (
-  <div>
-    <h3>Csaoka</h3>
-  </div>
-);
+const HomePage = () => {
+  const handleClick = () => {
+    axios.get("/ping").then(response => console.log(response));
+  };
+  return (
+    <div>
+      <h3>Start Poker session?</h3>
+      <button onClick={handleClick}>Create Game</button>
+    </div>
+  );
+};
 
 export default HomePage;
