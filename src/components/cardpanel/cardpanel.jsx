08f7ee11a2a2fbfc71e74f5d26385b7a003a143b
e-mog <modi.g@windowslive.com>
2019-04-10 11:27:52 +0200

    cardpanel with react hooks

--- src/components/cardpanel/cardpanel.jsx
+++ src/components/cardpanel/cardpanel.jsx
@@ -1,10 +1,50 @@
-import React, { Component } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import './card.scss';
 import SocketContext from './../App/socket-context';
 
 type Props = {
   className?: string
 };
+
+export default function Cardpanel(props: Props) {
+  const [cardlist] = useState(['1', '2', '3', '5', '8', '13', '21', '?', 'PASS']);
+  const [selected, setSelected] = useState('-1');
+
+  const socket = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.emit('greet', { message: 'Hello, I am cardpanel!' });
+  }, []);
+
+  const handleSelection = (n: string) => () => {
+    setSelected(n);
+  };
+  return (
+    <div>
+      <h1>Select a Card</h1>
+      <div className="panel">
+        {cardlist.map((c, i) => (
+          <div key={i} className="cardHolder">
+            <div onClick={handleSelection(c)} className={c === selected ? 'card selected' : 'card'}>
+              <svg viewBox="0 0 30 50" width="100%">
+                <text
+                  x="15"
+                  y="35"
+                  textAnchor="middle"
+                  textLength={c.length < 3 ? '0' : '30'}
+                  lengthAdjust="spacingAndGlyphs"
+                >
+                  {c}
+                </text>
+              </svg>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+/*
 type State = {
   cardlist: string[],
   selected: string,
@@ -55,3 +95,4 @@ export default class Cardpanel extends Component<Props, State> {
     );
   }
 }
+*/
